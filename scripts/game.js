7109917c2cb2cbc32380deb7a432c6473cc10e3c
Ahmad Ataya <ahmadataya1980@gmail.com>
2026-06-25 10:02:08 +0200

    Draw initial character image

--- scripts/game.js
+++ scripts/game.js
@@ -1,10 +1,22 @@
 ﻿let canvas;
 let ctx;
+let characterImage;
 
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
+    loadCharacterImage();
     console.log('Game initialized');
+}
+
+function loadCharacterImage() {
+    characterImage = new Image();
+    characterImage.src = './assets/img/2_character_pepe/2_walk/W-21.png';
+    characterImage.onload = drawCharacter;
+}
+
+function drawCharacter() {
+    ctx.drawImage(characterImage, 120, 180, 100, 250);
 }
 
 window.addEventListener('load', init);
