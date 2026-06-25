bbc1cfb4ec4660081ea773fcf6e34455abc680da
Ahmad Ataya <ahmadataya1980@gmail.com>
2026-06-25 10:04:26 +0200

    Add drawable object class

---
+++ classes/drawable-object.class.js
@@ -0,0 +1,16 @@
+﻿class DrawableObject {
+    x = 120;
+    y = 180;
+    width = 100;
+    height = 250;
+    img;
+
+    loadImage(path) {
+        this.img = new Image();
+        this.img.src = path;
+    }
+
+    draw(ctx) {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
+}
--- scripts/game.js
+++ scripts/game.js
@@ -1,22 +1,22 @@
 ﻿let canvas;
 let ctx;
-let characterImage;
+let character;
 
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
-    loadCharacterImage();
+    createCharacter();
     console.log('Game initialized');
 }
 
-function loadCharacterImage() {
-    characterImage = new Image();
-    characterImage.src = './assets/img/2_character_pepe/2_walk/W-21.png';
-    characterImage.onload = drawCharacter;
+function createCharacter() {
+    character = new DrawableObject();
+    character.loadImage('./assets/img/2_character_pepe/2_walk/W-21.png');
+    character.img.onload = drawCharacter;
 }
 
 function drawCharacter() {
-    ctx.drawImage(characterImage, 120, 180, 100, 250);
+    character.draw(ctx);
 }
 
 window.addEventListener('load', init);
