2b4344ec458e9cb6f56564cfdabf124ee62ef9df
Ahmad Ataya <ahmadataya1980@gmail.com>
2026-06-25 13:23:40 +0200

    chicken animation movement

--- classes/chicken.class.js
+++ classes/chicken.class.js
@@ -4,6 +4,11 @@
     y = 360;
     width = 70;
     height = 70;
+    IMAGES_WALKING = [
+        './assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
+        './assets/img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
+        './assets/img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
+    ];
 
     // #endregion
 
@@ -14,7 +19,8 @@
         super();
         this.x = x;
         this.speed = 0.6;
-        this.loadImage('./assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
+        this.loadImage(this.IMAGES_WALKING[0]);
+        this.loadImages(this.IMAGES_WALKING);
         this.animate();
     }
 
@@ -22,11 +28,11 @@
 
     // #region animation
 
-    // Moves the chicken continuously to the left.
+    // Moves and animates the chicken continuously.
     animate() {
         IntervalHub.setStoppableInterval(() => this.moveLeft(), 1000 / 60);
+        IntervalHub.setStoppableInterval(() => this.playAnimation(this.IMAGES_WALKING), 200);
     }
 
     // #endregion
 }
-
