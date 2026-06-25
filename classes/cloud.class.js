76d62c609a958a3a62b6404dc04f6463e6a4d830
Ahmad Ataya <ahmadataya1980@gmail.com>
2026-06-25 11:18:52 +0200

    Move clouds with interval hub

--- classes/cloud.class.js
+++ classes/cloud.class.js
@@ -4,6 +4,7 @@
     y = 20;
     width = 500;
     height = 250;
+    speed = 0.15;
 
     // #endregion
 
@@ -14,6 +15,16 @@
         super();
         this.x = x;
         this.loadImage('./assets/img/5_background/layers/4_clouds/1.png');
+        this.animate();
+    }
+
+    // #endregion
+
+    // #region animation
+
+    // Moves the cloud continuously to the left.
+    animate() {
+        IntervalHub.setStoppableInterval(() => this.moveLeft(), 1000 / 60);
     }
 
     // #endregion
