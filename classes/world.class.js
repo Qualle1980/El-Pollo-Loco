f9f704196f6d5b78467e33d2ae2cf56a6243410b
Ahmad Ataya <ahmadataya1980@gmail.com>
2026-06-25 10:10:50 +0200

    Add world class

---
+++ classes/world.class.js
@@ -0,0 +1,16 @@
+﻿class World {
+    character = new Character();
+    canvas;
+    ctx;
+
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.ctx = canvas.getContext('2d');
+        this.character.img.onload = () => this.draw();
+    }
+
+    draw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.character.draw(this.ctx);
+    }
+}
--- scripts/game.js
+++ scripts/game.js
@@ -1,21 +1,10 @@
 ﻿let canvas;
-let ctx;
-let character;
+let world;
 
 function init() {
     canvas = document.getElementById('canvas');
-    ctx = canvas.getContext('2d');
-    createCharacter();
+    world = new World(canvas);
     console.log('Game initialized');
-}
-
-function createCharacter() {
-    character = new Character();
-    character.img.onload = drawCharacter;
-}
-
-function drawCharacter() {
-    character.draw(ctx);
 }
 
 window.addEventListener('load', init);
