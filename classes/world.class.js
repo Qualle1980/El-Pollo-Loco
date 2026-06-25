92416f7be2105a0b26b7062d65adc114d989cd5d
Ahmad Ataya <ahmadataya1980@gmail.com>
2026-06-25 10:27:52 +0200

    Add layered background

--- classes/world.class.js
+++ classes/world.class.js
@@ -1,16 +1,37 @@
 ﻿class World {
     character = new Character();
+    backgroundObjects = [];
     canvas;
     ctx;
 
     constructor(canvas) {
         this.canvas = canvas;
         this.ctx = canvas.getContext('2d');
-        this.character.img.onload = () => this.draw();
+        this.createBackground();
+        this.draw();
+    }
+
+    createBackground() {
+        this.backgroundObjects = [
+            new BackgroundObject('./assets/img/5_background/layers/air.png', 0),
+            new BackgroundObject('./assets/img/5_background/layers/3_third_layer/1.png', 0),
+            new BackgroundObject('./assets/img/5_background/layers/2_second_layer/1.png', 0),
+            new BackgroundObject('./assets/img/5_background/layers/1_first_layer/1.png', 0)
+        ];
     }
 
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.addObjectsToMap(this.backgroundObjects);
         this.character.draw(this.ctx);
+        this.repeatDraw();
+    }
+
+    repeatDraw() {
+        requestAnimationFrame(() => this.draw());
+    }
+
+    addObjectsToMap(objects) {
+        objects.forEach((object) => object.draw(this.ctx));
     }
 }
