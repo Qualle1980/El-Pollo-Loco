011680b2981c60537ead78c18abfcd44b08960e9
Ahmad Ataya <ahmadataya1980@gmail.com>
2026-06-26 17:09:28 +0200

    Add small chicken enemy class

---
+++ classes/small-chicken.class.js
@@ -0,0 +1,21 @@
+class SmallChicken extends Chicken {
+    // #region properties
+
+    y = 370;
+    width = 50;
+    height = 50;
+    offset = {
+        top: 5,
+        right: 5,
+        bottom: 5,
+        left: 5
+    };
+    IMAGES_WALKING = [
+        './assets/img/3_enemies_chicken/chicken_small/1_walk/1_w.png',
+        './assets/img/3_enemies_chicken/chicken_small/1_walk/2_w.png',
+        './assets/img/3_enemies_chicken/chicken_small/1_walk/3_w.png'
+    ];
+    IMAGE_DEAD = './assets/img/3_enemies_chicken/chicken_small/2_dead/dead.png';
+
+    // #endregion
+}
